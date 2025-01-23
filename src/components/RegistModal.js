d4301ca524b0fa958be12e48e769a23057f65658
Mushofa20 <ahmadmushofasalsabil@gmail.com>
2025-01-23 21:51:34 +0700

    Update RegistModal.js
    add url backend

--- src/components/RegistModal.js
+++ src/components/RegistModal.js
@@ -17,7 +17,7 @@ const RegistModal = ({ show, handleClose }) => {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/register', {
+            const response = await fetch('https://mern-project1-backend-production.up.railway.app/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
