8ea9cecb7cd2c9d7d8ea8fb7ab103e586458383f
Mushofa20 <ahmadmushofasalsabil@gmail.com>
2025-02-25 14:42:39 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           <div className="col-lg-3 col-md-6 mb-5 mb-md-5">
             <div className="ftco-footer-widget mb-4" data-aos="fade-up" data-aos-delay="100">
               <h2 className="ftco-heading-2">About Us</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut risus eget erat luctus aliquet.</p>
               <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                 <li data-aos="fade-up" data-aos-delay="200"><a href="/"><span className="icon-twitter"></span></a></li>
                 <li data-aos="fade-up" data-aos-delay="200"><a href="/"><span className="icon-facebook"></span></a></li>
@@ -28,7 +28,7 @@ const Footer = () => {
               <div className="block-21 mb-4 d-flex">
                 <a href='/' className="blog-img mr-4" style={{ backgroundImage: `url(${image1})` }}></a>
                 <div className="text">
-                  <h3 className="heading"><a href="/">Even the all-powerful Pointing has no control about</a></h3>
+                  <h3 className="heading"><a href="/">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></h3>
                   <div className="meta">
                     <div><a href="/"><span className="icon-calendar"></span> Sept 15, 2018</a></div>
                     <div><a href="/"><span className="icon-person"></span> Admin</a></div>
@@ -39,7 +39,7 @@ const Footer = () => {
               <div className="block-21 mb-4 d-flex">
                 <a href='/' className="blog-img mr-4" style={{ backgroundImage: `url(${image2})` }}></a>
                 <div className="text">
-                  <h3 className="heading"><a href="/">Even the all-powerful Pointing has no control about</a></h3>
+                  <h3 className="heading"><a href="/">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></h3>
                   <div className="meta">
                     <div><a href="/"><span className="icon-calendar"></span> Sept 15, 2018</a></div>
                     <div><a href="/"><span className="icon-person"></span> Admin</a></div>
@@ -65,9 +65,9 @@ const Footer = () => {
             	<h2 className="ftco-heading-2">Have a Questions?</h2>
             	<div className="block-23 mb-3">
 	              <ul>
-	                <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="/"><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a></li>
-	                <li><a href="/"><span className="icon icon-envelope"></span><span className="text">info@yourdomain.com</span></a></li>
+	                <li><span className="icon icon-map-marker"></span><span className="text">Jl. Xxxxxx No. xx</span></li>
+	                <li><a href="/"><span className="icon icon-phone"></span><span className="text">+62 xxx xxxx xxxx</span></a></li>
+	                <li><a href="/"><span className="icon icon-envelope"></span><span className="text">info@domain.com</span></a></li>
 	              </ul>
 	            </div>
             </div>
@@ -76,10 +76,9 @@ const Footer = () => {
         <div className="row">
           <div className="col-md-12 text-center">
             <p>
-              Copyright &copy;{currentYear} All rights reserved | This template is made with{' '}
-              <i className="icon-heart" aria-hidden="true"></i> by{' '}
-              <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">
-                Colorlib
+              Copyright &copy;{currentYear} All rights reserved | This website is made with
+              <a href="https://abil-liart.vercel.app" target="_blank" rel="noopener noreferrer">
+                Abil
               </a>
             </p>
           </div>
